bff45c64014f7e7649362b57f01df91cc4708f04
Sankar <sankar@Sankars-MacBook-Pro.local>
2019-08-08 13:45:12 +0530

    Updates

---
+++ precache-manifest.3d966501ca77cade0652389572cb1063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f077c48f6c37b38f462e99337c5fca0",
+    "url": "sankarthiruganam.github.io/index.html"
+  },
+  {
+    "revision": "d783a3b21d90d87340bf",
+    "url": "sankarthiruganam.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "sankarthiruganam.github.io/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d783a3b21d90d87340bf",
+    "url": "sankarthiruganam.github.io/static/js/main.639f08fa.chunk.js"
+  },
+  {
+    "revision": "20aef6b6062b645e5679",
+    "url": "sankarthiruganam.github.io/static/js/runtime~main.526927a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "sankarthiruganam.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
